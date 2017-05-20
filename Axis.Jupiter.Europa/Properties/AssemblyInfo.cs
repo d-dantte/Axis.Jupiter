98fdca9636a0aee022be6e1b5f7f31c83e808e82
dantte <dev.bankai@gmail.com>
2017-05-20 03:05:34 +0100

    prepared AssemblyInfo.cs files from all projects

--- Axis.Jupiter.Europa/Properties/AssemblyInfo.cs
+++ Axis.Jupiter.Europa/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Axis.Jupiter.Europa")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("APIs enabling aggregated configuration of EF - allowing entities to be pulled from different assemblies in a predictable and convenient manner.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Axiis")]
 [assembly: AssemblyProduct("Axis.Jupiter.Europa")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
