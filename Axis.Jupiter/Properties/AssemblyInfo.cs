d99e492d5acef601cdb78ca5d843743260ba9751
dantte <dev.bankai@gmail.com>
2017-06-17 18:35:43 +0100

    changed assembly titles and stuff and removed kore

--- Axis.Jupiter/Properties/AssemblyInfo.cs
+++ Axis.Jupiter/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Axis.Jupiter.Kore")]
+[assembly: AssemblyTitle("Axis.Jupiter")]
 [assembly: AssemblyDescription("API abstracting Data Persistence Commands")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Axiis")]
-[assembly: AssemblyProduct("Axis.Jupiter.Kore")]
+[assembly: AssemblyProduct("Axis.Jupiter")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
