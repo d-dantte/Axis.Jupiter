1a74c7d188a1315273c6f9b70c7258c2cbc2a45a
Stanley Damasus <stanley.damasus@betssongroup.com>
2018-11-28 12:27:07 +0100

    completed jupiter and EFCore and Mongo. all are untested

--- Axis.Jupiter.Mongo/MongoStoreCommand.cs
+++ Axis.Jupiter.Mongo/MongoStoreCommand.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 using Axis.Jupiter.Contracts;
 using Axis.Luna.Extensions;
@@ -51,11 +50,18 @@ namespace Axis.Jupiter.Mongo
             _addToCollectionMethod = GetType().GetMethod(nameof(AddToCollection));
             _addRangeToCollectionMethod = GetType().GetMethod(nameof(AddRangeToCollection));
             _updateInCollectionMethod = GetType().GetMethod(nameof(UpdateInCollection));
+            _updateRangeInCollectionMethod = GetType().GetMethod(nameof(UpdateRangeInCollection));
+            _removeFromCollectionMethod = GetType().GetMethod(nameof(RemoveFromCollection));
+            _removeRangeFromCollectionMethod = GetType().GetMethod(nameof(RemoveRangeFromCollection));
         }
 
+        #region Add
         public Operation<Model> Add<Model>(Model model) 
         where Model : class => Operation.Try(async () =>
         {
+            if (model == null)
+                throw new ArgumentException("Invalid Model specified: null");
+
             var transformMap = _entityConfiguration
                 .ModelTransformer.Transforms()
                 .FirstOrDefault(map => map.ModelType == typeof(Model))
@@ -67,6 +73,26 @@ namespace Axis.Jupiter.Mongo
             await this.CallFunc<Task>(method, entity);
             return _entityConfiguration.ModelTransformer.ToModel<Model>(entity);
         });
+        
+        public Operation AddBatch<Model>(IEnumerable<Model> models)
+        where Model : class => Operation.Try(async () =>
+        {
+            if (models == null)
+                throw new ArgumentException("Invalid model list specified: null");
+
+            var transformMap = _entityConfiguration
+                .ModelTransformer.Transforms()
+                .FirstOrDefault(map => map.ModelType == typeof(Model))
+                .ThrowIfNull($"No Transform Map found for model type: {typeof(Model).FullName}");
+
+            //use fast-call to call the AddToCollection<Entity> method on the database object
+            var method = _addRangeToCollectionMethod.MakeGenericMethod(transformMap.EntityType);
+            var entities = models
+                .Where(model => model != null)
+                .Select(_entityConfiguration.ModelTransformer.ToEntity);
+
+            await this.CallFunc<Task>(method, entities.ToArray() as object); //so we take the entire array as one parameter into the method
+        });
 
         private async Task AddToCollection<Entity>(Entity entity)
         {
@@ -75,21 +101,6 @@ namespace Axis.Jupiter.Mongo
                 .GetCollection<Entity>(typeof(Entity).FullName)
                 .InsertOneAsync(entity);
         }
-        
-
-        public Operation AddBatch<Model>(IEnumerable<Model> models)
-        where Model : class => Operation.Try(async () =>
-        {
-            var transformMap = _entityConfiguration
-                .ModelTransformer.Transforms()
-                .FirstOrDefault(map => map.ModelType == typeof(Model))
-                .ThrowIfNull($"No Transform Map found for model type: {typeof(Model).FullName}");
-
-            //use fast-call to call the AddToCollection<Entity> method on the database object
-            var method = _addRangeToCollectionMethod.MakeGenericMethod(transformMap.EntityType);
-            var entities = models.Select(_entityConfiguration.ModelTransformer.ToEntity);
-            await this.CallFunc<Task>(method, entities.ToArray() as object); //so we take the entire array as one parameter into the method
-        });
 
         private async Task AddRangeToCollection<Entity>(Entity[] entity)
         {
@@ -98,24 +109,48 @@ namespace Axis.Jupiter.Mongo
                 .GetCollection<Entity>(typeof(Entity).FullName)
                 .InsertManyAsync(entity);
         }
+        #endregion
 
-
+        #region Update
         public Operation<Model> Update<Model>(Model model)
         where Model : class => Operation.Try(async () =>
         {
+            if (model == null)
+                throw new ArgumentException("Invalid Model specified: null");
+
             var transformMap = _entityConfiguration
                 .ModelTransformer.Transforms()
                 .FirstOrDefault(map => map.ModelType == typeof(Model))
                 .ThrowIfNull($"No Transform Map found for model type: {typeof(Model).FullName}");
 
             //use fast-call to call the AddToCollection<Entity> method on the database object
-            var method = _addToCollectionMethod.MakeGenericMethod(transformMap.EntityType);
+            var method = _updateInCollectionMethod.MakeGenericMethod(transformMap.EntityType);
             var entity = _entityConfiguration.ModelTransformer.ToEntity(model);
             await this.CallFunc<Task>(method, entity);
             return _entityConfiguration.ModelTransformer.ToModel<Model>(entity);
         });
 
-        public async Task UpdateInCollection<Entity>(Entity entity)
+        public Operation UpdateBatch<Model>(IEnumerable<Model> models)
+        where Model : class => Operation.Try(async () =>
+        {
+            if (models == null)
+                throw new ArgumentException("Invalid Model list specified: null");
+
+            var transformMap = _entityConfiguration
+                .ModelTransformer.Transforms()
+                .FirstOrDefault(map => map.ModelType == typeof(Model))
+                .ThrowIfNull($"No Transform Map found for model type: {typeof(Model).FullName}");
+
+            //use fast-call to call the AddToCollection<Entity> method on the database object
+            var method = _updateRangeInCollectionMethod.MakeGenericMethod(transformMap.EntityType);
+            var entities = models
+                .Where(model => model != null)
+                .Select(_entityConfiguration.ModelTransformer.ToEntity);
+
+            await this.CallFunc<Task>(method, entities.ToArray() as object); //so we take the entire array as one parameter into the method
+        });
+
+        private async Task UpdateInCollection<Entity>(Entity entity)
         {
             var info = _entityConfiguration
                 .EntityInfoMap
@@ -134,20 +169,128 @@ namespace Axis.Jupiter.Mongo
                 .ReplaceOneAsync(filter, entity);
         }
 
-
-        public Operation UpdateBatch<Model>(IEnumerable<Model> d) where Model : class
+        /// <summary>
+        /// Perform a bulk replace
+        /// </summary>
+        /// <typeparam name="Entity"></typeparam>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        private async Task UpdateRangeInCollection<Entity>(Entity[] entities)
         {
-            throw new NotImplementedException();
+            var info = _entityConfiguration
+                .EntityInfoMap
+                .InfoFor<Entity>();
+
+            var replaceModels = entities
+                .Select(entity =>
+                {
+                    var keyValues = info
+                        .KeyProperties
+                        .Select(propName => entity.PropertyValue(propName))
+                        .ToArray();
+
+                    return new ReplaceOneModel<Entity>(info.IdentityFilter<Entity>(keyValues), entity);
+                });
+
+            var result = await _client
+                .GetDatabase(_databaseLabel, _settings)
+                .GetCollection<Entity>(typeof(Entity).FullName)
+                .BulkWriteAsync(replaceModels, new BulkWriteOptions{ BypassDocumentValidation =  false });
+
+            //for now, not sure what to use the result for, so i'll ignore...
+        }
+        #endregion
+
+        #region Delete
+        public Operation<Model> Delete<Model>(Model model)
+        where Model : class => Operation.Try(async () =>
+        {
+            if (model == null)
+                return null;
+
+            var transformMap = _entityConfiguration
+                .ModelTransformer.Transforms()
+                .FirstOrDefault(map => map.ModelType == typeof(Model))
+                .ThrowIfNull($"No Transform Map found for model type: {typeof(Model).FullName}");
+
+            //use fast-call to call the AddToCollection<Entity> method on the database object
+            var method = _removeFromCollectionMethod.MakeGenericMethod(transformMap.EntityType);
+            var entity = _entityConfiguration.ModelTransformer.ToEntity(model);
+            await this.CallFunc<Task>(method, entity);
+            return _entityConfiguration.ModelTransformer.ToModel<Model>(entity);
+        });
+
+        public Operation DeleteBatch<Model>(IEnumerable<Model> models)
+        where Model : class => Operation.Try(async () =>
+        {
+            if (models == null)
+                throw new ArgumentException("Invalid Model list specified: null");
+
+            var transformMap = _entityConfiguration
+                .ModelTransformer.Transforms()
+                .FirstOrDefault(map => map.ModelType == typeof(Model))
+                .ThrowIfNull($"No Transform Map found for model type: {typeof(Model).FullName}");
+
+            //use fast-call to call the AddToCollection<Entity> method on the database object
+            var method = _removeRangeFromCollectionMethod.MakeGenericMethod(transformMap.EntityType);
+            var entities = models
+                .Where(model => model != null)
+                .Select(_entityConfiguration.ModelTransformer.ToEntity);
+
+            await this.CallFunc<Task>(method, entities.ToArray() as object); //so we take the entire array as one parameter into the method
+        });
+        
+
+        private async Task RemoveFromCollection<Entity>(Entity entity)
+        {
+            var info = _entityConfiguration
+                .EntityInfoMap
+                .InfoFor<Entity>();
+
+            var keyValues = info
+                .KeyProperties
+                .Select(propName => entity.PropertyValue(propName))
+                .ToArray();
+
+            var filter = info.IdentityFilter<Entity>(keyValues);
+
+            await _client
+                .GetDatabase(_databaseLabel, _settings)
+                .GetCollection<Entity>(typeof(Entity).FullName)
+                .DeleteOneAsync(filter);
         }
 
-        public Operation<Model> Delete<Model>(Model d) where Model : class
+        /// <summary>
+        /// Perform a bulk replace
+        /// </summary>
+        /// <typeparam name="Entity"></typeparam>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        private async Task RemoveRangeFromCollection<Entity>(Entity[] entities)
         {
-            throw new NotImplementedException();
-        }
+            var info = _entityConfiguration
+                .EntityInfoMap
+                .InfoFor<Entity>();
 
-        public Operation DeleteBatch<Model>(IEnumerable<Model> d) where Model : class
-        {
-            throw new NotImplementedException();
+            var filter = entities
+                .Select(entity =>
+                {
+                    var keyValues = info
+                        .KeyProperties
+                        .Select(propName => entity.PropertyValue(propName))
+                        .ToArray();
+
+                    return info.IdentityFilter<Entity>(keyValues);
+                })
+                .Pipe(Builders<Entity>.Filter.Or);
+
+            var result = await _client
+                .GetDatabase(_databaseLabel, _settings)
+                .GetCollection<Entity>(typeof(Entity).FullName)
+                .DeleteManyAsync(filter);
+
+            //for now, not sure what to use the result for, so i'll ignore...
         }
+        #endregion
     }
 }
---
+++ Axis.Jupiter.New4J.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Axis.Jupiter.New4J.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
